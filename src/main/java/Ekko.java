9fcdf91f1a8312d6085bdd8ebfe41f4a698a6699
joshua <joshua@kata.ai>
2025-01-23 16:54:57 +0800

    Level 3. Mark as Done

--- src/main/java/Ekko.java
+++ src/main/java/Ekko.java
@@ -2,7 +2,34 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Ekko {
-    public static ArrayList<String> list = new ArrayList<>();
+    public static ArrayList<Task> list = new ArrayList<>();
+
+    public static class Task {
+        protected String description;
+        protected boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "X" : " "); // mark done task with X
+        }
+
+        public void markDone() {
+            this.isDone = true;
+        }
+
+        public void markUndone() {
+            this.isDone = false;
+        }
+
+        @Override
+        public String toString() {
+            return this.description;
+        }
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -26,13 +53,36 @@ public class Ekko {
                 System.out.println(
                         "____________________________________________________________");
                 for (int i = 0; i < list.size(); i++) {
-                    System.out.println(" " + (i + 1) + ". " + list.get(i));
+                    Task task = list.get(i);
+                    System.out.println(" " + (i + 1) + ". " + "[" +  task.getStatusIcon() + "]" + " " + task);
                 }
 
                 System.out.println(
                         "____________________________________________________________");
-            } else {
-                list.add(input);
+            } else if (input.matches("mark \\d+")) {
+                System.out.println(
+                        "____________________________________________________________\n" +
+                        "Nice! I've marked this task as done:");
+                Integer i = Integer.parseInt(input.split(" ")[1]) - 1;
+                Task task = list.get(i);
+                task.markDone();
+                System.out.println("[" +  task.getStatusIcon() + "]" + " " + task);
+
+                System.out.println(
+                        "____________________________________________________________");
+            } else if (input.matches("unmark \\d+")) {
+                System.out.println(
+                        "____________________________________________________________\n" +
+                        "OK, I've marked this task as not done yet:");
+                Integer i = Integer.parseInt(input.split(" ")[1]) - 1;
+                Task task = list.get(i);
+                task.markUndone();
+                System.out.println("[" +  task.getStatusIcon() + "]" + " " + task);
+
+                System.out.println(
+                        "____________________________________________________________");
+            }else {
+                list.add(new Task(input));
                 System.out.println(
                         "____________________________________________________________\n" +
                         " added: " + input + "\n" +
