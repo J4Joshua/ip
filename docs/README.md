e12fff2d9679c99b39ff1bcb4331690877601876
joshua <joshua@kata.ai>
2025-01-23 10:56:18 +0800

    Level 0. Rename, Greet, Exit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Ekko User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Ekko.java
@@ -0,0 +1,12 @@
+public class Ekko {
+    public static void main(String[] args) {
+
+        System.out.println(
+                "____________________________________________________________\n" +
+                " Hello! I'm Ekko\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n");
+    }
+}
